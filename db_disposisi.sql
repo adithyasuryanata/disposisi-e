dfcb475789c6cacf601ffb407f4d19df232d2b4f
adithyasuryanata <adithyasuryanata@gmail.com>
2024-01-14 13:30:04 +0800

    second commit

--- db_disposisi.sql
+++ db_disposisi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 03, 2024 at 02:04 PM
+-- Generation Time: Jan 14, 2024 at 06:28 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -20,6 +20,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_disposisi`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_surat_masuk`
+--
+
+CREATE TABLE `tb_surat_masuk` (
+  `id` int(11) NOT NULL,
+  `no_surat` varchar(100) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `surat_from` varchar(100) NOT NULL,
+  `surat_to` varchar(100) NOT NULL,
+  `tgl_terima` timestamp NULL DEFAULT current_timestamp(),
+  `perihal` text NOT NULL,
+  `keterangan` text NOT NULL,
+  `image` varchar(255) NOT NULL DEFAULT 'no_image.jpg',
+  `is_active` int(2) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_surat_masuk`
+--
+
+INSERT INTO `tb_surat_masuk` (`id`, `no_surat`, `tgl_surat`, `surat_from`, `surat_to`, `tgl_terima`, `perihal`, `keterangan`, `image`, `is_active`) VALUES
+(1, '01/I/2024', '2024-01-14', 'SMK 2 Banjarmasin', 'Pimpinan PT', '2024-01-13 16:00:00', 'Pengajuan Dana', 'Bantuan Sosial', '01.png', 1),
+(2, '02/I/2024', '2024-01-14', 'PT. KDP', 'Pimpinan PT. Sukses Lestari', '2024-01-13 16:00:00', 'Pengajuan Kerjasama', 'Kerjasama Dalam Rangka Sumber Daya Karyawan', '02.png', 1),
+(3, '03/I/2024', '2024-01-16', 'adsasdas', 'asdasda', '2024-01-15 16:00:00', 'Kerjasama Tingkat Lanjut', 'Kunjungan Sekolah', '3sd.jpg', 1),
+(4, '04/I/2024', '2024-01-15', 'PT. KDP', 'PT. Agro Lestari', '2024-01-14 16:00:00', 'Permohonan Bantuan', 'Permohonan Bantuan', '7.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -44,12 +73,18 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`id`, `username`, `password`, `email`, `full_name`, `phone`, `role`, `last_login`, `is_active`) VALUES
-(1, 'admin', '$2y$10$sSitIy9UrELame87IVZQFero9UebzWiQ88JEdYXcmwjeCn16r4Qdq', 'adithya@gmail.com', 'Adithya Suryanata Winardi', '089523236137', 'admin', '2024-01-03 13:03:11', 1),
-(2, 'Sya', '$2y$10$/s3gGNMdmoFG7v6bqdGsneKr9PMx3eGvQejEQ/1pGtZRuqZFbRwle', 'sya@gmail.com', 'Syaaa', '089537128123', 'sekretaris', '2024-01-03 13:04:35', 1);
+(1, 'Adithya', '$2y$10$BL1jXvcO5rmS071OtZQ.ueA5twvYLPQim/27FSduMmuDHQZxNz.Vm', 'adithya@gmail.com', 'Adithya Suryanata Winardi', '089523236137', 'admin', '2024-01-14 04:50:47', 1),
+(2, 'Sya', '$2y$10$hnQNP5Um8ZPC51C2VKCtzOlKhll28vyPnuE/iELJ3loV9TZ34nfjK', 'sya@gmail.com', 'Syaaa', '089537128123', 'sekretaris', '2024-01-14 04:50:52', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tb_surat_masuk`
+--
+ALTER TABLE `tb_surat_masuk`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tb_user`
@@ -60,6 +95,12 @@ ALTER TABLE `tb_user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `tb_surat_masuk`
+--
+ALTER TABLE `tb_surat_masuk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_user`
